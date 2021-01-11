01afe5796974bf26fff627ba395b386917cca342
Marechal <alexmarechal00@gmail.com>
2021-01-11 20:50:19 +0100

    Create home route

---
+++ src/Controller/MainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class MainController extends AbstractController {
+
+  /**
+     * @Route("/", name="home")
+     */
+    public function home(){
+      return $this->render('main/home.html.twig');
+    }
+  
+}
